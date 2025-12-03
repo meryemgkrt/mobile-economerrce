f72911ccf9d4120f170d69f55f0a12c07588a6f8
meryemgkrt <meryemkurt824@gmail.com>
2025-12-03 23:56:50 +0300

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,14 +5,17 @@ const app = express();
 
 const __dirname = path.resolve();
 
+// API routes
 app.get("/api/health", (req,res)=>{
-    res.status(200).json
-({message:"Merhaba kod çalıştı!:))"});})
+    res.status(200).json({message:"Merhaba kod çalıştı!:))"});
+})
 
+// Production - Admin panel
 if(ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../admin/dist")));
     
-    app.get("/{*any}", (req,res)=>{
+    // Catch-all route - en sona koy
+    app.use((req,res)=>{
         res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
     })
 }
