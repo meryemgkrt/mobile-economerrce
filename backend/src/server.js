1618fd7df32b936ec88ebda5a198ca26495376d5
meryemgkrt <meryemkurt824@gmail.com>
2025-12-03 22:50:50 +0300

    mobile, admin page add

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import {ENV} from './config/env.js';
+const app = express();
+
+const __dirname = path.resolve();
+
+app.get("/api/health", (req,res)=>{
+    res.status(200).json
+({message:"Merhaba kod çalıştı!:))"});})
+
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../admin/dist")));
+    
+    app.get("/{*any}", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
+    })
+}
+
+app.listen(ENV.PORT, ()=>console.log(`Merhaba sunucu çalıştı! Port: ${ENV.PORT}`));
