1dc8635be632bd5f3a028d5433f42d1ea41dcac1
meryemgkrt <meryemkurt824@gmail.com>
2025-12-05 15:02:23 +0300

    server upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import path from 'path';
-import {ENV} from './config/env.js';
-const app = express();
+import { ENV } from './config/env.js';
 
+const app = express();
 const __dirname = path.resolve();
 
 // API routes
-app.get("/api/health", (req,res)=>{
-    res.status(200).json({message:"Merhaba kod çalıştı!:))"});
-})
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Merhaba kod çalıştı!:))" });
+});
 
-// Serve admin panel (always in production)
-app.use(express.static(path.join(__dirname,"../admin/dist")));
+// Serve admin panel (production)
+app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-app.use((req,res)=>{
-    res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
-})
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+});
 
-app.listen(ENV.PORT || 10000, ()=>console.log(`Sunucu çalıştı! Port: ${ENV.PORT || 10000}`));
+app.listen(ENV.PORT || 10000, () =>
+  console.log(`Sunucu çalıştı! Port: ${ENV.PORT || 10000}`)
+);
