07f5e3c0fe2d3967a0244b045b6426c03f8af59c
meryemgkrt <meryemkurt824@gmail.com>
2025-12-07 18:07:15 +0300

    server.js upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,25 @@
 import express from "express";
+import path from "path";
+import {ENV} from "./config/env.js";
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
-  res.send("Backend is up and running 🚀");
-});
+app.get("/api/health", (req, res)=>{
+    res.status(200).json({message: "Merhaba kod çalıştı!:))"});
+})
 
-app.get("/api/health", (req, res) => {
-  res.status(200).json({ message: "Merhaba kod çalıştı!:))" });
-});
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../admin/dist")))
 
-app.listen(PORT, () => {
+    app.get("/{*any}", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+    })
+}
+
+a
+
+app.listen(ENVPORT, () => {
   console.log(`Sunucu çalıştı! Port: ${PORT}`);
 });
