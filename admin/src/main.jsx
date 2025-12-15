9918a70641370a6ea12ca9a63d38687a0d4eeba3
meryemgkrt <meryemkurt824@gmail.com>
2025-12-15 16:26:04 +0300

    admin upload

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const PUBLISHABLE_KEY=import.meta.VITE_CLERK_PUBLISHABLE_KEY
-if(!PUBLISHABLE_KEY){
-  throw new Error("VITE_CLERK_PUBLISHABLE_KEY is not defined")
+const key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if(!key) {
+  throw new Error('VITE_CLERK_PUBLISHABLE_KEY is not defined')
 }
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
