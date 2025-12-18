d281541e74e5e2031735d2e2994e4366077d2ca7
meryemgkrt <meryemkurt824@gmail.com>
2025-12-18 11:09:26 +0300

    db.js upload

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import {ENV} from "./env.js";
 
 export const connectDB=async()=>{
   try {
-    const conn =await mongoose.connect(ENV.DB_URL)
+    const conn =await mongoose.connect(ENV.MONGODB_URI)
     console.log(`👍 🚀 MongoDB connected: ${conn.connection.host}`);
     
   } catch (error) {
