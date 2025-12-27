f0aaa47b54f4b1ddb9af3df861c737b796c0c56c
meryemgkrt <meryemkurt824@gmail.com>
2025-12-27 23:33:13 +0300

    server upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ENV.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
