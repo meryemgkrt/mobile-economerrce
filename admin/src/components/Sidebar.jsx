f721e26afb9cbc62d8e3d8c0d2cf1a3b30f52b3b
meryemgkrt <meryemkurt824@gmail.com>
2025-12-30 16:49:57 +0300

    admin upload

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,15 +1,26 @@
 import { useUser } from "@clerk/clerk-react";
-import { lİNK,useLocation } from "react-router";
-import {NAVİGATION} from "./Navbar";
+import { Link,useLocation } from "react-router";
+import {NAVIGATION} from "./Navbar";
+import { ShoppingBagIcon } from "lucide-react";
+
 
 function Sidebar() {
   const location = useLocation();
   const {user} =useUser();
 
-  console.log("user",user);
+  
   return (
     <div>
-        Sidebar
+        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay">
+
+        </label>
+        <div className="p-4 w-full ">
+          <div className="flex items-center gap-3">
+            <div className="size-10 bg-primary rounded-xl flex items-center justify-center shrink-0">
+              <ShoppingBagIcon className="w-6 h-6 text-primary-content"/>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
