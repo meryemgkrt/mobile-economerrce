26024c2ef185cdb23b90db362f88591387996b22
meryemgkrt <meryemkurt824@gmail.com>
2025-12-30 16:36:03 +0300

    admin upload

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { UserButton } from "@clerk/clerk-react";
+import { useLocation } from "react-router";
 
+
+import { ClipboardListIcon, PanelLeftIcon, HomeIcon, ShoppingCartIcon, UserIcon } from "lucide-react"
+
+export const NAVIGATION = [
+  { name: "Dashboard", path: "/dashboard", icon: <HomeIcon className='size-5' /> },
+  { name: "Products", path: "/products", icon: <ClipboardListIcon className='size-5' /> },
+  { name: "Orders", path: "/orders", icon: <ShoppingCartIcon className='size-5' /> },
+  { name: "Customers", path: "/customer", icon: <UserIcon className='size-5' /> },
+]
 function Navbar() {
+  const location = useLocation();
+
   return (
-    <div>Navbar</div>
+    <div className="navbar w-full bg-base-300">
+      <label htmlFor="my-drawer" className="btn btn-square btn-ghost" aria-label="open sidebar">
+        <PanelLeftIcon className="size-5" />
+      </label>
+      <div className="flex-1 px-4">
+        <h1 className="text-xl font-bold">
+          {NAVIGATION.find(nav => nav.path === location.pathname)?.name || "Dashboard"}
+        </h1>
+      </div>
+      <div className="mr-5">
+        <UserButton afterSignOutUrl="/login" />
+      </div>
+    </div>
   )
 }
-
-export default Navbar
+export default Navbar;
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useUser } from "@clerk/clerk-react";
+import { lİNK,useLocation } from "react-router";
+import {NAVİGATION} from "./Navbar";
 
 function Sidebar() {
+  const location = useLocation();
+  const {user} =useUser();
+
+  console.log("user",user);
   return (
     <div>
         Sidebar
