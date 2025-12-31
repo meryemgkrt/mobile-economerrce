7ef7a828b5cc33bfde36a4ceae9a7756d018085a
meryemgkrt <meryemkurt824@gmail.com>
2025-12-31 23:12:04 +0300

    server  upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -69,6 +69,9 @@ app.use(
 /* ============================
    TEST INNGEST
 ============================ */
+/* ============================
+   TEST INNGEST
+============================ */
 app.post("/api/test-inngest", async (req, res) => {
   try {
     await inngest.send({
