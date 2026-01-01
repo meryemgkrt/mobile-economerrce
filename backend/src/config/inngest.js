90b8632b81c1fc2dfda265545c24749778ba9e2f
meryemgkrt <meryemkurt824@gmail.com>
2026-01-01 23:09:11 +0300

    inngest upload

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,49 +1,38 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
-import User from "../models/user.model.js";
-import { ENV } from "./env.js";
+import  User  from "../models/user.model.js";
 
-export const inngest = new Inngest({
-  id: "economerrce-app",
-  signingKey: ENV.INNGEST_SIGNING_KEY,
-  signingKeyFallback: ENV.INNGEST_SIGNING_KEY_FALLBACK,
-});
+export const inngest = new Inngest({ id: "ecommerce-app" });
 
 const syncUser = inngest.createFunction(
   { id: "sync-user" },
   { event: "clerk/user.created" },
   async ({ event }) => {
     await connectDB();
-
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
     const newUser = {
       clerkId: id,
-      email: email_addresses?.[0]?.email_address || "",
-      name: `${first_name || ""} ${last_name || ""}`.trim() || "User",
-      imageUrl: image_url || "",
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "User",
+      imageUrl: image_url,
       addresses: [],
       wishlist: [],
     };
 
-    await User.updateOne(
-      { clerkId: id },
-      { $setOnInsert: newUser },
-      { upsert: true }
-    );
-
-    return { ok: true };
+    await User.create(newUser);
   }
 );
 
-const deleteUserFromDb = inngest.createFunction(
+const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
     await connectDB();
-    await User.deleteOne({ clerkId: event.data.id });
-    return { ok: true };
+
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
   }
 );
 
-export const functions = [syncUser, deleteUserFromDb];
+export const functions = [syncUser, deleteUserFromDB];
